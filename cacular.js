71585616f2e7ab4afad74ce95dba9fbcf14551b3
jhosoan cordeiro onofre <81454435+jhosoan@users.noreply.github.com>
2022-02-20 20:10:41 -0300

    code

---
+++ cacular.js
@@ -0,0 +1,12 @@
+resultado = document.getElementById("Mostrar")
+zerar = document.getElementById("c")
+
+function click_um(){
+    n1 = 1111;n2 = 0;
+    
+    s = n1 + n2 
+    resultado.innerHTML = `${s}`
+}
+function click_zero(){
+    resultado.innerText = `0`
+} 
